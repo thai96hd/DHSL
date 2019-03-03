8fb6b9b9d42dd3aa6246df4c30bacb288f03d534
DESKTOP-2PFPM2E\TranThai <thaitran101096@gmail.com>
2019-03-03 09:53:16 +0700

    first commit

---
+++ DHSL/DHSL/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DHSL.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DHSL/DHSL/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DHSL.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
